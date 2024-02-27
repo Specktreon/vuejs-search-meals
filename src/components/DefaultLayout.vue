000cddaa04903b7cb07e85b568883d8c4d7e057c
Specktreon <lezzar37@gmail.com>
2024-02-27 08:30:28 +0100

    working on SearchByName

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-100 h-full">
+    <navbar />
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from "../components/Navbar.vue";
+</script>
+
+<script>
+// You can leave this empty if you don't need to declare any additional logic or options
+</script>
---
+++ src/components/GuestLayout.vue
@@ -0,0 +1,7 @@
+<template>Login Page</template>
+
+<script setup></script>
+
+<script>
+// You can leave this empty if you don't need to declare any additional logic or options
+</script>
